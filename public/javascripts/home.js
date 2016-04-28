7b5a266be2fb7521ac6d1aec8721f1b254edfd1c
Akshay Durge <akshay.durge@shadelinks.com>
2016-04-28 23:30:32 +0530

    added smiley images

--- public/javascripts/home.js
+++ public/javascripts/home.js
@@ -8,9 +8,9 @@ function generate_grid(resultJson) {
     }else if(resultJson.status === 'FAILED'){
         isFail = true;
     }
-    for(var j = 0;j < resultJson.dimension; j++){
+    for(var j = 0;j < resultJson.rows; j++){
         theTable += '<tr>';
-        for(var k = 0; k < resultJson.dimension; k++){
+        for(var k = 0; k < resultJson.columns; k++){
             id = j + '_' + k;
             cell = resultJson.grid.cells[j][k];
             if(isFail){
@@ -26,9 +26,11 @@ function generate_grid(resultJson) {
         }
         theTable += '</tr>';
     }
+
     $('#minesweeper_table').empty();
     $('#minesweeper_table').append(theTable);
     if(isFail){
+        $('#generate_panel_icon').attr('src','/assets/images/sad.png');
         for (var i = mines.length - 1; i >= 0; i--) {
             id = mines[i];
             $('#' + id).removeClass('visible');
@@ -36,6 +38,7 @@ function generate_grid(resultJson) {
         }
         $('#minesweeper_table').find("button").attr("disabled", "disabled"); //Disable table
     }else if(isSuccess){
+        $('#generate_panel_icon').attr('src','/assets/images/happy.png');
         $('.winner_alert').fadeIn();
         $('#minesweeper_table').find("button").attr("disabled", "disabled"); //Disable table
         $('#fireworks_container').show();
@@ -48,8 +51,8 @@ function generate_grid(resultJson) {
 }
 
 function generate_submit_json(resultJson) {
-    for(var j = 0;j < resultJson.panel.dimension; j++){
-        for(var k = 0; k < resultJson.panel.dimension; k++){
+    for(var j = 0;j < resultJson.panel.rows; j++){
+        for(var k = 0; k < resultJson.panel.columns; k++){
             var id = j + '_' + k;
             var tableCell = document.getElementById(id);
             if (tableCell.classList.contains("visible")) {
